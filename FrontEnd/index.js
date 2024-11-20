fd8f426d16ca8915fa04820cdf0c0d291dbface1
dwicks2113 <dwicks2113@outlook.com>
2024-11-20 11:28:04 -0500

    Work on filter buttons and code cleanup

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,32 +5,25 @@ document.addEventListener("DOMContentLoaded", function(){
   const btnContainer = document.getElementById('btn-container');
   const gallery = document.querySelector('.gallery')
 
-   
-  // let btnContainer = document.getElementById('btn-container');
-  // const catName = document.createElement('category.name');
-  // const btnID = document.createElement('filter-button');
+  
 
 //create buttons for categories and assign category ID number to each button
-// document.addEventListener("DOMContentLoaded", function() {
+
 const work = [
   { category: { name: 'All', btnID: 0 } },
   { category: { name: 'Objects', btnID: 1} },
-  { category: { name: 'Hotels and Restaurants', btnID: 3} },
-  { category: { name: 'Apartments', btnID: 2} }
- 
+  { category: { name: 'Apartments', btnID: 2} },
+  { category: { name: 'Hotels and Restaurants', btnID: 3} }
 ];
-
-
 
 // For each unique category, create a button
 work.forEach(item => {
-  // const btnContainer = document.getElementById('btn-container')
+
+  const btnContainer = document.getElementById('btn-container')
   const category = item.category.name; // Initialize 'category' here
   const button = document.createElement('button');
   button.className = 'button';
-  // button.style.backgroundColor = 'white';
-  // button.style.margin = '2em';
-  // button.style.borderRadius = '30%';
+  
   button.textContent = category;
   button.dataset.catID = item.category.btnID;
 
@@ -55,8 +48,6 @@ fetch('http://localhost:5678/api/works')
     return response.json()
   })
   .then(works => {
-    // console.log(works)
-    // for each item in array,
     const categories = new Set(work, 'category') // Use a Set to store unique category IDs
     // Loop through each work and add its category ID to the Set
     works.forEach(work => {
@@ -98,51 +89,15 @@ function createGalleryItem (work) {
   }
  }
 
-function filterGallery(catID) {
+function filterGallery(btnID) {
   const figures = gallery.querySelectorAll('figure');
   figures.forEach(figure => {
-    if(catID===0 || figure.dataset.catID == catID) {
+    if(btnID===0 || figure.dataset.btnID == btnID) {
       figure.style.display = 'block';
     } else {
     figure.style.display = 'none';
    }
   });
-}
 
-
-
-  
-// // POST request using fetch with error handling
-// const element = document.querySelector(
-//   '#post-request-error-handling .article-id'
-// )
-// const requestOptions = {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify({ title: 'Fetch POST Request Example' })
-// }
-// fetch('https://reqres.in/invalid-url', requestOptions)
-//   .then(async response => {
-//     const isJson = response.headers
-//       .get('content-type')
-//       ?.includes('application/json')
-//     const data = isJson && (await response.json())
-
-//     // check for error response
-//     if (!response.ok) {
-//       // get error message from body or default to response status
-//       const error = (data && data.message) || response.status
-//       return Promise.reject(error)
-//     }
-
-//     element.innerHTML = data.id
-//   })
-//   .catch(error => {
-//     element.parentElement.innerHTML = `Error: ${error}`
-//     console.error('There was an error!', error)
-//   })
-
-
- 
-
+  }
 })
