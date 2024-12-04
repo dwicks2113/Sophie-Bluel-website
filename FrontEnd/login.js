d427c0b56ca5485189c55167eaddd09800385c8a
dwicks2113 <dwicks2113@outlook.com>
2024-12-04 13:54:49 -0500

    Got login to work

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,7 +1,8 @@
 //Login Page Authentication
 document.addEventListener("DOMContentLoaded", function() {
   const loginButton = document.getElementById('login-button');
-  loginButton.addEventListener('click', function(event) {
+
+    loginButton.addEventListener('click', function(event) {
     event.preventDefault();
 
     const email = document.getElementById('email').value;
@@ -12,17 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
         alert('Please enter a valid email address.');
         return;
     }
-    if(!validatePassword(password)) 
-        alert('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.');
-        return;    
-});
+    // if(!validatePassword(password)) 
+    //     alert('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.');
+    //     return;    
+
 
     const payload = {
       email: email,
       password: password
     };
 
-  fetch('http://localhost:5678/api/login', {
+  fetch('http://localhost:5678/api/users/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -39,10 +40,8 @@ document.addEventListener("DOMContentLoaded", function() {
     
     .then(data => {
       if(data.token) {
-        
         localStorage.setItem('token', data.token);
         console.log('Login successful. Token stored in local storage.')
-
         window.location.href = 'index.html';
        } else {
         alert('login failed: ' + (data.message || 'Unknown error'));
@@ -53,7 +52,8 @@ document.addEventListener("DOMContentLoaded", function() {
       alert('Login failed: ' + error.message);
     });
   });
-  
+}) 
+ 
   //validate email
   function validateEmail(email) {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
